c8a727eebf52a6e801445ee653a133acffb8e1a7
Abhishek Dam <damforwork247@gmail.com>
2026-08-18 15:51:40 +0530

    first commit

---
+++ src/main/java/com/acciojob/bookmyshow_august_2026/BookmyshowAugust2026Application.java
@@ -0,0 +1,13 @@
+package com.acciojob.bookmyshow_august_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyshowAugust2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyshowAugust2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/acciojob/bookmyshow_august_2026/BookmyshowAugust2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acciojob.bookmyshow_august_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyshowAugust2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
